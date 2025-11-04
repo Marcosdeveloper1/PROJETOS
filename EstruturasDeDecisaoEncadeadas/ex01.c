0a75b8f5aef61a912122e4160a0d6e1f3d8ae273
Marcos Levi <levimarcos213@gmail.com>
2025-11-03 22:31:13 -0300

    atividades de Estruturas de decisão encadeadas

---
+++ EstruturasDeDecisaoEncadeadas/ex01.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade;
+    printf("Digite a sua idade: \n");
+    scanf("%d", &idade);
+
+    if (idade < 12){
+        printf("Você é uma criança!\n");
+    }else if(idade >= 12 && idade < 18){
+        printf("Você é um adolescente!\n");
+    }else if(idade >= 18 && idade < 60){
+        printf("Você é um adulto\n");
+    }
+    else{
+        printf("Você é um idoso\n");
+    }
+
+}
+
+//Criança < 12
+//Adolescente 12 <= x < 18
+//Adulto 18 <= x < 60
+//Idoso > 60
+
+//idoso, adulto, adolescente, criança
