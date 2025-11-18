5615f6d8aa973c113169fc542aba458b8ce4abc5
Marcos Levi <levimarcos213@gmail.com>
2025-11-18 19:27:22 -0300

    update

---
+++ OperadoresTernarios/ex01.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int temperatura = 28;
+    int resultado;
+
+    temperatura > 30 ? 1: 0; //1 para verdadeiro e 0 para falso
+    if(temperatura == 1)
+    {
+        printf("Está calor!\n");
+    }else{
+        printf("Está frio!\n");
+    }
+    return 0;
+
+}
